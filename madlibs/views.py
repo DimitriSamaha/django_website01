858d415a325f10642cedaf7f7215289e7272d311
DimitriSamaha <dimitri.samaha@gmail.com>
2022-01-09 12:24:58 +0100

    Last Madlibs touches
    Changed message if inputs are empty.

--- madlibs/views.py
+++ madlibs/views.py
@@ -4,14 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     if request.method == "POST":
+        inputs = []
 
         adj = request.POST["adjective"]
+        inputs.append(adj)
         verb1 = request.POST["verb1"]
+        inputs.append(verb1)
         verb2 = request.POST["verb2"]
+        inputs.append(verb2)
         famous_person = request.POST["famous_person"]
+        inputs.append(famous_person)
 
-        result = f"Computer programming is so {adj}! It makes me so excited all the time because" \
-                 f"I love to {verb1}. Stay positive and {verb2} like you are {famous_person}"
+        for input in inputs:
+            if input == "":
+                inputs.remove(input)    
+        if len(inputs) != 4 :
+            result = "Please fill all the form, Thank you!"
+        else:
+            result = f"Computer programming is so {adj}! It makes me so excited all the time because" \
+                     f"I love to {verb1}. Stay positive and {verb2} like you are {famous_person}"
         
         return render(request, "madlibs/index.html", {
             "message": result
